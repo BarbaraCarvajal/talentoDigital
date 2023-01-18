4731655482b9692e6253319999ecab652e9f9bcc
Barbara Carvajal <barb.carvajalsaez@gmail.com>
2023-01-18 16:39:34 -0300

    ejemplo

---
+++ Modulo_2/src/RepasosEnClases/Ejemplo.java
@@ -0,0 +1,10 @@
+package RepasosEnClases;
+
+public class Ejemplo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("¿qué haces acá?");
+	}
+
+}
