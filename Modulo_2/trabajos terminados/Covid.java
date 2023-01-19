9ee4dee9b568650c7169b24b9119c02e9991e74c
Barbara Carvajal <barb.carvajalsaez@gmail.com>
2023-01-19 19:24:40 -0300

    trabajos

--- Modulo_2/trabajos terminados/Covid.java
+++ Modulo_2/trabajos terminados/Covid.java
@@ -4,24 +4,18 @@ import java.util.Scanner;
 
 public class Covid {
 
+	
+	public static void pcr() {
+		System.out.println("¿cuál ha sido la respuesta del PCR? POSITIVO = 1 | NEGATIVO = 2");
+		int respuestaPcr = teclado.nextInt();
+		if (respuestaPcr == 1){
+			System.out.println("Tendrás que hacer cuarentena de 14 días :C");
+		}else if (respuesta == 2) {
+			System.out.println("Eres libre de irte, no necesitas cuarentena :D");
+		}
+	}
 	public static void main(String[] args) {
-		/*
-		 * 
-		 * Considere el siguiente enunciado. Determine las unidades al mínimo nivel de detalle, y 
-         * expréselas como un conjunto de expresiones lógicas.
-		 * 
-		 * “Una persona debe permanecer 14 días en observación únicamente si ha salido del país. 
-		 *  La persona se hará el PCR si quiere volver a la normalidad. 
-		 *  Dado que ni la persona ha 
-		 *  salido del país ni presenta contagios cercanos, no tendrá que hacer cuarentena ni se 
-		 *  le hará el PCR.”
-		 * 
-		 *  Recuerde asociar cada frase atómica como una variable, y asignarle un valor de verdad cualquiera,
-		 *  siempre que aplique al caso. Una vez realizado, evalúe la situación con al menos dos combinaciones 
-		 *  de valores de verdad iniciales, y verifique los cambios en el resultado final.
-		 * 
-		 * */
-		
+	
 		
 		Scanner teclado = new Scanner(System.in);
 		
@@ -33,7 +27,17 @@ public class Covid {
 		System.out.println("¿Haz salido del país? (1=Si | 2=No");
 		int respuesta = teclado.nextInt();
 		if (respuesta == 1) {
-			System.out.println("Como haz salido del país, tendrás que hacerte un PCR");
+			System.out.println("Haz salido del país, tendrás que hacerte un PCR");
+			pcr();
+			
+		}else if(respuesta == 2) {
+			System.out.println("¿Haz tenido contacto estrecho con una persona contagiada con el COVID? SI = 1 | NO = 2");
+			int respuestaContacto = teclado.nextInt();
+			if (respuestaContacto == 1) {
+				System.out.println("Debes hacerte pcr preventino");
+				pcr();
+				
+			}
 		}
 	}
 
